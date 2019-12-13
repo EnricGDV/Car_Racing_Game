aa04423282c1686641dbfbb0bc4cedddd36dce2a
AlexLA99 <45202060+AlexLA99@users.noreply.github.com>
2019-12-13 14:06:09 +0100

    more and more roads

--- Physics3D_class5 - solution/ModuleSceneIntro.cpp
+++ Physics3D_class5 - solution/ModuleSceneIntro.cpp
@@ -22,13 +22,22 @@ bool ModuleSceneIntro::Start()
 	App->camera->LookAt(vec3(0, 0, 0));
 
 	CreateLine({ 0.0f, 0.0f, -10.0f }, { 0.0f, 0.0f, 20.0f }, 16, true);
+	CreateLine({ -48.0f, 0.0f, -10.0f }, { -48.0f, 0.0f, 20.0f }, 16, true);
+	CreateLine({ 0.0f, 0.0f, -30.0f }, { 0.0f, 0.0f, -60.0f }, 16, true);
+	CreateLine({ -48.0f, 0.0f, -30.0f }, { -48.0f, 0.0f, -60.0f }, 16, true);
 	CreateLine({ 48.0f, 0.0f, -10.0f }, { 48.0f, 0.0f, 20.0f }, 16, true);
+	CreateLine({ 48.0f, 0.0f, -30.0f }, { 48.0f, 0.0f, -60.0f }, 16, true);
 	CreateLine({ 48.0f, 0.0f, 40.0f }, { 48.0f, 0.0f, 70.0f }, 16, true);
 	CreateLine({ 48.0f, 0.0f, 70.0f }, { 48.0f, 0.0f, 100.0f }, 16, true);
 	CreateLine({ 48.0f, 0.0f, 100.0f }, { 48.0f, 0.0f, 130.0f }, 16, true);
 	CreateLine({ 58.0f, 0.0f, 30.0f }, { 88.0f, 0.0f, 30.0f }, 16, true);
-	CreateLine({ 58.0f, 0.0f, 30.0f }, { 88.0f, 0.0f, 30.0f }, 16, true);
+	CreateLine({ 58.0f, 0.0f, -70.0f }, { 88.0f, 0.0f, -70.0f }, 16, true);
 	CreateLine({ 10.0f, 0.0f, 30.0f }, { 40.0f, 0.0f, 30.0f }, 16, true);
+	CreateLine({ 10.0f, 0.0f, -70.0f }, { 40.0f, 0.0f, -70.0f }, 16, true);
+	CreateLine({ 10.0f, 0.0f, -20.0f }, { 40.0f, 0.0f, -20.0f }, 16, true);
+	CreateLine({ 58.0f, 0.0f, -20.0f }, { 88.0f, 0.0f, -20.0f }, 16, true);
+	CreateLine({ -10.0f, 0.0f, -70.0f }, { -40.0f, 0.0f, -70.0f }, 16, true);
+	CreateLine({ -10.0f, 0.0f, -20.0f }, { -40.0f, 0.0f, -20.0f }, 16, true);
 	CreateLine({ 10.0f, 0.0f, 140.0f }, { 40.0f, 0.0f, 140.0f }, 16, true);
 	CreateLine({ 58.0f, 0.0f, 140.0f }, { 88.0, 0.0f, 140.0f }, 16, true);
 	CreateLine({ 58.0f, 0.0f, 140.0f }, { 88.0, 0.0f, 140.0f }, 16, true);
@@ -37,6 +46,7 @@ bool ModuleSceneIntro::Start()
 	CreateLine({ -10.0f, 0.0f, 30.0f }, { -40.0f, 0.0f, 30.0f }, 16, true);
 
 	CreateLine({ 8.0f, 0.0f, 30.0f }, { -22.0f, 0.0f, 30.0f }, 16, false);
+	CreateLine({ 0.0f, 0.0f, 70.0f }, { 0.0f, 0.0f, 40.0f }, 16, false);
 
 	return ret;
 }
@@ -69,13 +79,13 @@ void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
 }
 
 
-void ModuleSceneIntro::CreateLine(const vec3 i, const vec3 f, uint numCubes, bool isDouble)
+void ModuleSceneIntro::CreateLine(const vec3 initialPos, const vec3 finalPos, uint numCubes, bool isDouble)
 {
 
-	float distance = sqrt(pow(f.x - i.x, 2) + pow(f.y - i.y, 2) + pow(f.z - i.z, 2));
-	float dist_segment = distance / numCubes;
+	float distance = sqrt(pow(finalPos.x - initialPos.x, 2) + pow(finalPos.y - initialPos.y, 2) + pow(finalPos.z - initialPos.z, 2));
+	float distanceSeg = distance / numCubes;
 
-	vec3 dir_v = f - i;
+	vec3 dir_v = finalPos - initialPos;
 	float dir_v_mod = sqrt((dir_v.x*dir_v.x) + (dir_v.y*dir_v.y) + (dir_v.z * dir_v.z));
 	dir_v /= dir_v_mod;
 	vec3 perp_v = { -dir_v.z, 0, dir_v.x };
@@ -86,21 +96,21 @@ void ModuleSceneIntro::CreateLine(const vec3 i, const vec3 f, uint numCubes, boo
 	vec3 dim(1, 2, 1);
 
 	Cube wall;
-	wall.color = Blue;
+	wall.color = White;
 	wall.size = { dim.x, dim.y, dim.z };
 
 	for (uint j = 0; j < numCubes; j++)
 	{
 		if (isDouble)
 		{
-			pos = (i + (dir_v * j * dist_segment)) + ((TRACK_WIDTH / 2) * perp_v);
+			pos = (initialPos + (dir_v * j * distanceSeg)) + ((TRACK_WIDTH / 2) * perp_v);
 			wall.SetPos(pos.x, pos.y + 1, pos.z);
 			cube_pieces.primitive_bodies.PushBack(wall);
 			cube_pieces.physic_bodies.PushBack(App->physics->AddBody(wall, 0.0f));
 		}
 		
 		
-		pos = (i + (dir_v * j * dist_segment)) + ((TRACK_WIDTH / 2) * -perp_v);
+		pos = (initialPos + (dir_v * j * distanceSeg)) + ((TRACK_WIDTH / 2) * -perp_v);
 		wall.SetPos(pos.x, pos.y + 1, pos.z);
 		cube_pieces.primitive_bodies.PushBack(wall);
 		cube_pieces.physic_bodies.PushBack(App->physics->AddBody(wall, 0.0f));
